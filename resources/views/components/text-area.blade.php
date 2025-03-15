1756d261a494771b10946c67811875804152bdbc
lucaslopesllima <lucaslopesllima@gmail.com>
2025-03-15 15:53:56 -0300

    Adjust style on text compoenents

--- resources/views/components/text-area.blade.php
+++ resources/views/components/text-area.blade.php
@@ -1,4 +1,4 @@
 @props(['disabled' => false])
-<textarea rows="17" @disabled($disabled) {{ $attributes->merge(['class' => 'border border-gray-300 dark:border-white p-4 rounded-lg']) }}>
+<textarea style="border: 1px solid  #cecece;" rows="17" @disabled($disabled) {{ $attributes->merge(['class' => 'border border-gray-300 dark:border-white p-4 rounded-lg']) }}>
 {{ $slot }}
 </textarea>
--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) }}>
+<input  style="border: 1px solid  #cecece;" @disabled($disabled) {{ $attributes->merge(['class' => 'focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) }}>
