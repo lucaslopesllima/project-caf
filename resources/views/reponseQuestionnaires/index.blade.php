72d1b29f2719583dc897299ed1ba885967241669
lucaslopesllima <lucaslopesllima@gmail.com>
2025-04-13 20:17:55 -0300

    statarted new template

--- resources/views/reponseQuestionnaires/index.blade.php
+++ resources/views/reponseQuestionnaires/index.blade.php
@@ -1,20 +1,24 @@
 <x-app-layout>
     <main class="w-full">
-        <h2 class="text-2xl font-bold ms-10 mt-10 mb-4 ms-3">Cadastro de Questionarios</h2>
-        <a class="btn btn-primary ms-3" href="{{ route('questionario.create') }}">Cadastrar</a>
+        <h2 class="text-2xl font-bold ms-10 mt-10 mb-4 ms-3">Escolha um questionario</h2>
+        <a class="btn btn-primary ms-3" href="{{ route('questionario.create') }}">Nova Resposta</a>
         <div class="overflow-auto ms-10 me-10">
             <table class="table w-full text-left">
                 <thead>
                     <tr>
-                        <th class="w-1/6 px-4 py-2">Pergunta</th>
+                        <th class="w-1/6 px-4 py-2">Nome Questionario</th>
+                        <th class="w-1/6 px-4 py-2">Pesoa Responsável</th>
                         <th class="w-1/6 px-4 py-2">Ação</th>
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach ($questionnaires as $questionnaire)
+                    @foreach ($answers as $questionnaire)
                         <tr class="hover:bg-gray-900/50">
                             <td class="px-4 py-2 truncate">
-                                {{$questionnaire->nome}}
+                                {{$questionnaire->questionnaire_name}}
+                            </td>
+                            <td class="px-4 py-2 truncate">
+                                {{$questionnaire->person_name}}
                             </td>
                             <td class="flex">
                                 <a href="{{ route('questionario.edit',['questionario'=>$questionnaire->id]) }}" class="btn btn-ghost btn-xs">Editar</a>
@@ -32,7 +36,7 @@
                 <tfoot>
                     <tr>
                         <td>
-                            @include('paginator', ['paginator' => $questionnaires])
+                            @include('paginator', ['paginator' => $answers])
                         </td>
                     </tr>
                 </tfoot>
