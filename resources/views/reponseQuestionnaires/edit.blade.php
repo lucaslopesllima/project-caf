37151e0b4476782f384ee6c40f5541640ebf96d4
lucaslopesllima <lucaslopesllima@gmail.com>
2025-04-13 21:35:46 -0300

    Adjusted edit template

--- resources/views/reponseQuestionnaires/edit.blade.php
+++ resources/views/reponseQuestionnaires/edit.blade.php
@@ -1,15 +1,42 @@
 <x-app-layout>
-    <x-backward-button href="{{ route('questionario.index') }}"></x-backward-button>
-    <section class="d-flex justify-center align-center">
-    <header class="d-flex justify-center align-center">
-            <h2 class="text-lg font-medium  text-base-content">
-                {{ __('Update the questionnaire') }}
-            </h2>
-    </header>
-    <form method="post" action="{{ route('questionario.update',['questionario']) }}" class="mt-6 space-y-6">
-        @csrf
-        @method('patch')
-        @include('questionnaire.partials.form')
-    </form>
-</section>
+    <x-backward-button href="{{ route('solved_questionnairies') }}"></x-backward-button>
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <section class="d-flex justify-center align-center">
+                <header class="d-flex justify-center align-center">
+                    <h2 class="text-lg font-medium text-base-content">
+                        {{ __('Update the answers') }}
+                    </h2>
+                </header>
+                <form method="post" action="{{ route('pessoa-questionario.update', $pessoaQuestionario->id) }}" class="mt-6 space-y-6">
+                    @csrf
+                    @method('PUT')
+                    
+                    <div class="mb-4">
+                        <p class="text-base-content">
+                            <strong>{{ __('Person') }}:</strong> {{ $pessoaQuestionario->pessoa->nome }}
+                        </p>
+                        <p class="text-base-content">
+                            <strong>{{ __('Questionnaire') }}:</strong> {{ $pessoaQuestionario->questionario->nome }}
+                        </p>
+                    </div>
+
+                    @foreach($perguntas as $pergunta)
+                        <div>
+                            <x-input-label :value="$pergunta->texto" />
+                            <x-text-input 
+                                type="text" 
+                                name="respostas[{{ $pergunta->id }}]" 
+                                class="mt-1 block w-full"
+                                :value="isset($respostas[$pergunta->id]) ? $respostas[$pergunta->id]->texto : ''"
+                                required
+                            />
+                        </div>
+                    @endforeach
+
+                    <div class="flex items-center justify-center gap-4">
+                        <x-primary-button>{{ __('Save') }}</x-primary-button>
+                    </div>
+                </form>
+            </section>
+        </div>
 </x-app-layout>
