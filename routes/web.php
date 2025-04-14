c1d8f8b1e570ef1bff00a32dbdc6f19f110e60b2
lucaslopesllima <lucaslopesllima@gmail.com>
2025-04-13 21:36:23 -0300

    Created new routes

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\PessoaController;
 use App\Http\Controllers\PessoaQuestionarioController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\QuestionarioController;
+use App\Models\PessoaQuestionario;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -21,15 +22,16 @@ Route::post('register', [RegisteredUserController::class, 'store']);
 Route::middleware('auth')->group(function () {
 
     Route::resources([
-        'profile'=>ProfileController::class,
-        'pergunta'=>PerguntaController::class,
-        'pessoa'=>PessoaController::class,
-        'questionario'=>QuestionarioController::class
+        'profile'             => ProfileController::class,
+        'pergunta'            => PerguntaController::class,
+        'pessoa'              => PessoaController::class,
+        'questionario'        => QuestionarioController::class,
+        'pessoa-questionario' => PessoaQuestionarioController::class 
     ]);
 
     Route::get('responder-questionario',
     [PessoaQuestionarioController::class,'index'])
-    ->name('solve_questionnaire');
+    ->name('solved_questionnairies');
 
     Route::get('/dashboard', function () {
         return view('dashboard');
@@ -39,6 +41,10 @@ Route::middleware('auth')->group(function () {
         Auth::logout();
         return redirect('/');
     })->name('logout');
+
+    Route::get('pessoa-questionario/{id}/answers-data', 
+        [PessoaQuestionarioController::class, 'getAnswersData'])
+        ->name('pessoa-questionario.answers-data');
 });
 
 require __DIR__.'/auth.php';
