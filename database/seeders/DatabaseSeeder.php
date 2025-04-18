8d5b5d22eb6d4b2ee10944ae670ccdd6b8fffbf9
lucaslopesllima <lucas.lopes@sci.com.br>
2025-04-18 17:26:10 -0300

    Fix: Updated .gitignore to remove .vscode and enhanced DatabaseSeeder with additional seeders. Expanded RespostaSeeder with new response options for improved data diversity.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,12 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         \App\Models\User::factory()->count(10)->create();
+        $this->call(PessoaSeeder::class);
+        $this->call(PerguntaSeeder::class);
+        $this->call(QuestionarioSeeder::class);
+        $this->call(PerguntaQuestionarioSeeder::class);
+        $this->call(RespostaSeeder::class);
+        $this->call(PessoaQuestionarioSeeder::class);
+
     }
 }
