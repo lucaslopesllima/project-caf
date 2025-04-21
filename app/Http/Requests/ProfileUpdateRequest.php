effd71ffd2c590dba1c69b22719a21bed99af491
lucaslopesllima <lucas.lopes@sci.com.br>
2025-04-20 23:22:40 -0300

    Adjuted user request validate

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Requests;
 
-use App\Models\User;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules;
 
 class ProfileUpdateRequest extends FormRequest
 {
@@ -23,8 +23,10 @@ class ProfileUpdateRequest extends FormRequest
                 'lowercase',
                 'email',
                 'max:255',
-                Rule::unique(User::class)->ignore($this->user()->id),
+                Rule::unique('users')->ignore($this->route('profile')),
             ],
+            'password' => ['nullable', 'confirmed', 'min:8', Rules\Password::defaults()],
+            'role' => ['required'],
         ];
     }
 }
