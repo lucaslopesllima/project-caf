2454e241efaadf89be93f2f286d9fdd0f804e5a1
lucaslopesllima <lucas.lopes@sci.com.br>
2025-04-21 00:34:36 -0300

    Adjuted questionnaire layout

--- resources/views/reponseQuestionnaires/create.blade.php
+++ resources/views/reponseQuestionnaires/create.blade.php
@@ -1,25 +1,13 @@
 <x-app-layout>
     <x-backward-button href="{{ route('questionario.index') }}"></x-backward-button>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <section class="d-flex justify-center align-center">
-                <header class="d-flex justify-center align-center">
-                    <h2 class="text-lg font-medium  text-base-content">
-                        Responda o questionario
-                    </h2>
-                </header>
-                <form method="post" action="{{ route('questionario.store') }}" class="mt-6 space-y-6">
-                    @csrf
-                    @method('post')
-                    @include('reponseQuestionnaires.partials.newAnswer')
-                </form>
-            </section>
-        </div>
+    <h2 class="text-lg font-medium text-base-content font-semibold text-xl leading-tight ms-7">
+        Responda o questionario
+    </h2>
+    <div class="max-w-7xl sm:px-6 lg:px-8 space-y-6">
+       <form method="post" action="{{ route('questionario.store') }}" class="mt-6 space-y-6">
+           @csrf
+           @method('post')
+           @include('reponseQuestionnaires.partials.newAnswer')
+       </form>
     </div>
 </x-app-layout>
