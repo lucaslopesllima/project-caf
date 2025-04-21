44f60e5fcadb67addaaa207d813102b289ed56ae
lucaslopesllima <lucas.lopes@sci.com.br>
2025-04-21 16:42:31 -0300

    Remove unsed route

--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PerguntaQuestionarioController;
-use App\Http\Controllers\PessoaController;
 
 Route::post('/pergunta-questionario/{pergunta_id}/{questionario_id}', [PerguntaQuestionarioController::class, 'store'])
     ->name('pergunta-questionario.store'); 
 
 Route::get('/pergunta-questionario/{id}', [PerguntaQuestionarioController::class, 'store'])
     ->name('pergunta-questionario.destroy'); 
-
-
-Route::get('pessoas_questionario',[PessoaController::class,'getPeople']);
