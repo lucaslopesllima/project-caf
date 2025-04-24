d8469e1d61969dcd491f062e05efeea0a4a3ea19
lucaslopesllima <lucaslopesllima@gmail.com>
2025-04-23 21:24:45 -0300

    Refactor dashboard components to improve layout and functionality. Update statistics cards for better data presentation and enhance chart integration for monthly beneficiary registrations. Optimize theme selector for a more intuitive user experience.

---
+++ database/migrations/2025_04_23_211835_add_queue_columns.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('pessoas', function (Blueprint $table) {
+            $table->tinyInteger('priority');
+            $table->tinyInteger('was_attended');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('pessoas', function (Blueprint $table) {
+            $table->dropColumn('priority');
+            $table->dropColumn('was_attended');
+        });
+    }
+};
